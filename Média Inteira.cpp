7e8a8e5215ccc91582d6b9c6a0f1d2d7b0564bf0
RechargeCG <109256805+RechargeCG@users.noreply.github.com>
2023-05-05 07:56:37 -0300

    05-05-23

---
+++ Média Inteira.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Lendo a entrada do exercício
+    int A, B;
+    cin >> A >> B;
+
+    cout << (int) (A+B)/2 << endl;
+
+    return 0;
+}
