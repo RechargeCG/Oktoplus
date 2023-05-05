c63099b4ddd983415409ae9106fd3d07846df2b8
RechargeCG <109256805+RechargeCG@users.noreply.github.com>
2023-05-05 07:38:07 -0300

    Create Weird Algorithm.cpp

---
+++ Weird Algorithm.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    long long N;
+    cin >> N;
+ 
+    while (N!=1)
+    {
+        cout << N << ' ';
+        if(N%2==0) N/=2;
+        else N=N*3+1;
+    }
+ 
+    cout << N << '\n';
+    
+ 
+    return 0;
+}
