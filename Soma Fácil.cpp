e90e810a4c3819d41b3c3182ae64781a5273537e
RechargeCG <109256805+RechargeCG@users.noreply.github.com>
2023-05-05 07:55:38 -0300

    05-05-23

---
+++ Soma Fácil.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // Lendo a entrada do exercício
+	int A, B;
+	scanf("%d",&A);
+	scanf("%d",&B);
+
+    printf("%d\n",(A+B));
+
+	return 0;
+}
