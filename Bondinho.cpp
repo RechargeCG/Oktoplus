edb7e3d858a0c72098da3b0ce122840104fee0f1
RechargeCG <109256805+RechargeCG@users.noreply.github.com>
2023-05-05 07:58:29 -0300

    05-05-23

--- Bondinho.cpp
+++ Bondinho.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    // Lendo a entrada do exercício
+    int A, M;
+    cin >> A >> M;
+
+    if(A+M<=50) cout << 'S' << endl;
+	else cout << 'N' << endl;
+
+    return 0;
+}
